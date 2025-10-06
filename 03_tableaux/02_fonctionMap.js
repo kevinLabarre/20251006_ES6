e5dcd4b8d9fa6f929e07b7e2a7c400389beeaf46
klabarre <klabarre@dawan.fr>
2025-10-06 14:09:33 +0200

    exemple .map()

--- 03_tableaux/02_fonctionMap.js
+++ 03_tableaux/02_fonctionMap.js
@@ -7,11 +7,29 @@ const persons = [
   { firstName: "Jayne", lastName: "Cobb" },
 ];
 
+const obj = { firstName: "Malcom", lastName: "Reynolds" };
+
 const firstNames = persons.map((element) => element.firstName);
 
 const firstNames2 = persons.map((element) => {
-  return { firstName: element.firstName };
+  return {
+    name: element.firstName,
+    fullName: `${element.firstName} ${element.lastName}`,
+  };
+  //   return { firstName: element.firstName };
 });
 
 console.log(firstNames);
 console.log(firstNames2);
+
+const chiffres = [1, 2, 3, 4, 5];
+
+const chiffresCarre = chiffres.map((chiffre) => chiffre * chiffre);
+console.log(chiffresCarre);
+
+// FlatMap()
+const chiffresCarre2 = chiffres.map((chiffre) => [chiffre * chiffre]);
+console.log(chiffresCarre2);
+
+// const faltMapCarre = chiffres.flatMap((chiffre) => [chiffre * chiffre]);
+// console.log(faltMapCarre);
